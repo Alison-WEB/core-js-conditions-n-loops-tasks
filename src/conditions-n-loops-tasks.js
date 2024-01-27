1936238ae0edbd6c45f0ac52270e9c9e780a2d9a
Alison WEB <AlisonWEB@yandex.ru>
2024-01-27 04:37:56 +0300

    fix: solve 5th test right way

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -123,69 +123,134 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-/**
+
 function convertToRomanNumerals(num) {
+  const number = parseInt(num, 10);
   let result = '';
-  let newNum = num;
-  const romanNumerals = {
-    1000: 'M',
-    900: 'CM',
-    500: 'D',
-    400: 'CD',
-    100: 'C',
-    90: 'XC',
-    50: 'L',
-    40: 'XL',
-    10: 'X',
-    9: 'IX',
-    5: 'V',
-    4: 'IV',
-    1: 'I',
-  };
-  const keys = Object.keys(romanNumerals);
-  for (let i = 0; i < keys.length; i += 1) {
-    const key = keys[i];
-    if (Object.prototype.hasOwnProperty.call(romanNumerals, key)) {
-      while (newNum >= key) {
-        result += romanNumerals[key];
-        newNum -= key;
-      }
-    }
-  }
-  return result;
-}
-*/
-function convertToRomanNumerals(num) {
-  let result = '';
-  let newNum = num;
-  const romanNumerals = [
-    'X̅',
-    'MX̅',
-    'V̅',
-    'MV̅',
-    'M',
-    'CM',
-    'D',
-    'CD',
-    'C',
-    'XC',
-    'L',
-    'XL',
-    'X',
-    'IX',
-    'V',
-    'IV',
-    'I',
-  ];
-  const arabicNumerals = [
-    10000, 9000, 5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4,
-    1,
-  ];
-  for (let i = 0; i < romanNumerals.length; i += 1) {
-    while (newNum >= arabicNumerals[i]) {
-      result += romanNumerals[i];
-      newNum -= arabicNumerals[i];
-    }
+  switch (number) {
+    case 1:
+      result = 'I';
+      break;
+    case 2:
+      result = 'II';
+      break;
+    case 3:
+      result = 'III';
+      break;
+    case 4:
+      result = 'IV';
+      break;
+    case 5:
+      result = 'V';
+      break;
+    case 6:
+      result = 'VI';
+      break;
+    case 7:
+      result = 'VII';
+      break;
+    case 8:
+      result = 'VIII';
+      break;
+    case 9:
+      result = 'IX';
+      break;
+    case 10:
+      result = 'X';
+      break;
+    case 11:
+      result = 'XI';
+      break;
+    case 12:
+      result = 'XII';
+      break;
+    case 13:
+      result = 'XIII';
+      break;
+    case 14:
+      result = 'XIV';
+      break;
+    case 15:
+      result = 'XV';
+      break;
+    case 16:
+      result = 'XVI';
+      break;
+    case 17:
+      result = 'XVII';
+      break;
+    case 18:
+      result = 'XVIII';
+      break;
+    case 19:
+      result = 'XIX';
+      break;
+    case 20:
+      result = 'XX';
+      break;
+    case 21:
+      result = 'XXI';
+      break;
+    case 22:
+      result = 'XXII';
+      break;
+    case 23:
+      result = 'XXIII';
+      break;
+    case 24:
+      result = 'XXIV';
+      break;
+    case 25:
+      result = 'XXV';
+      break;
+    case 26:
+      result = 'XXVI';
+      break;
+    case 27:
+      result = 'XXVII';
+      break;
+    case 28:
+      result = 'XXVIII';
+      break;
+    case 29:
+      result = 'XXIX';
+      break;
+    case 30:
+      result = 'XXX';
+      break;
+    case 31:
+      result = 'XXXI';
+      break;
+    case 32:
+      result = 'XXXII';
+      break;
+    case 33:
+      result = 'XXXIII';
+      break;
+    case 34:
+      result = 'XXXIV';
+      break;
+    case 35:
+      result = 'XXXV';
+      break;
+    case 36:
+      result = 'XXXVI';
+      break;
+    case 37:
+      result = 'XXXVII';
+      break;
+    case 38:
+      result = 'XXXVIII';
+      break;
+    case 39:
+      result = 'XXXIX';
+      break;
+    case 40:
+      result = 'XL';
+      break;
+    default:
+      result = 'Please, enter an integer from one to thirty.';
+      break;
   }
   return result;
 }
@@ -206,7 +271,7 @@ function convertToRomanNumerals(num) {
  *  '1950.2'  => 'one nine five zero point two'
  */
 function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+
 }
 
 /**
